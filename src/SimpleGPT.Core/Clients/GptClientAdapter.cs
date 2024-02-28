bede7ab986be9e851aa930ddf9e6a2824ccf09a4
tmenezes <thiagomenezes2k7@gmail.com>
2024-02-28 21:49:04 +0100

    Introduced client factory

---
+++ src/SimpleGPT.Core/Clients/GptClientAdapter.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleGPT.Core.Clients;
+
+public class GptClientAdapter : IGptClient
+{
+    private readonly ChatGptClient _chatGptClient;
+
+    public GptClientAdapter(string apiKey) : this(Constants.ModelNames.Gpt4Turbo, apiKey) { }
+    public GptClientAdapter(string modelName, string apiKey)
+    {
+        _chatGptClient = new ChatGptClient(modelName, apiKey);
+    }
+
+    public Task<string> CallGpt(string userInput)
+    {
+        _chatGptClient.AddChatMessage(ChatMessageType.User, userInput);
+        return _chatGptClient.CallGpt();
+    }
+}
---
+++ src/SimpleGPT.Core/GptClientFactory.cs
@@ -0,0 +1,20 @@
+﻿using SimpleGPT.Core.Clients;
+
+namespace SimpleGPT.Core;
+
+public interface IGptClientFactory
+{
+    IGptClient Create(string modelName, string apiKey);
+}
+
+public class GptClientFactory : IGptClientFactory
+{
+    public IGptClient Create(string apiKey) => Create(Constants.ModelNames.Gpt4Turbo, apiKey);
+    public IGptClient Create(string modelName, string apiKey)
+    {
+        var isGpt3Turbo = modelName.StartsWith("gpt-3.5-turbo");
+        return isGpt3Turbo
+            ? new GptCompletionsClient(modelName, apiKey)
+            : new GptClientAdapter(modelName, apiKey);
+    }
+}
